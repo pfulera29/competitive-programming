44fdee981f0694e508c105f15afa585517571489
pfulera29 <65029186+pfulera29@users.noreply.github.com>
2020-09-21 18:37:25 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,13 +1,25 @@
-int P[N], S[N];
+class DSU {
+ public:
+ 	vector <int> P, S;
 
-int froot(int x) {
-    return x == P[x] ? x : (P[x] = froot(P[x]));
-}
+ 	DSU(int n) {
+ 		P.resize(n+2);
+ 		S.resize(n+2);
+ 		for(int i = 0; i <= n; i++) {
+ 		  P[i] = i;
+ 		  S[i] = 1;
+ 		}
+ 	}
+ 	
+	int froot(int x) {
+	    return x == P[x] ? x : (P[x] = froot(P[x]));
+	}
 
-void union_(int x, int y) {
-    x = froot(x), y = froot(y);
-    if(x == y) return;
-    if (S[x] < S[y]) swap(x, y);
-    P[y] = x;
-    S[x] += S[y];
-}
+	void union_(int x, int y) {
+	    x = froot(x), y = froot(y);
+	    if(x == y) return;
+	    if (S[x] < S[y]) swap(x, y);
+	    P[y] = x;
+	    S[x] += S[y];
+	}
+};
