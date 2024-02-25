7333815ecf5478afea1c437164510d605e6c5dfe
pfulera29 <lit2018012@iiitl.ac.in>
2024-02-25 18:27:31 +0000

    add main file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    cout << "done\n";
+}
+
+signed main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+}
